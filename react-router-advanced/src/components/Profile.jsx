76494a8bc5ed2d2929602563d8a1fe38feaaa048
Manar Habib <manar7abib@gmail.com>
2024-11-24 18:47:46 +0200

    advanced

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
     <div>
-      <h1>Profile Page</h1>
+      <h1>Profile</h1>
       <nav>
-        <Link to="details">Profile Details</Link> | <Link to="settings">Profile Settings</Link>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
       </nav>
+
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+
       <Outlet />
     </div>
   );
