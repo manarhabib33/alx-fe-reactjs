812fe8d43a555969e51c9d448c67aa838126be96
Manar Habib <manar7abib@gmail.com>
2024-11-24 18:50:15 +0200

    router advanced

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,33 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/Home"; // Existing component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
-import ProtectedRoute from "./components/ProtectedRoute";
-import BlogPost from "./components/BlogPost";
+import BlogPost from "./components/BlogPost"; // Import for dynamic routing
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-        <Route path="/blog/:postId" element={<BlogPost />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <div>
+        <h1>Welcome to the Advanced React Router Application</h1>
+        <Routes>
+          {/* Existing Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/*" element={<Profile />} />
+
+          {/* Dynamic Route for Blog Posts */}
+          <Route path="/blog/:id" element={<BlogPost />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
 
 export default App;
+
